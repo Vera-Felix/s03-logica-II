59f223a66a402d3826b35f10e75af1ccc3310805
Vera-Felix <verafelix37@gmail.com>
2024-06-20 12:18:27 -0300

    Exercicio casa corrigido

--- exercicios_casa/custo_viagem_def_try.py
+++ exercicios_casa/custo_viagem_def_try.py
@@ -2,24 +2,24 @@ print ("Bem-vindo ao nosso sistema de cálculo de custo de viagem.")
 
 # entradas
 nome = input("Qual seu nome? ")
+# variáveis
+viagem = 20
 
 try:
     km_viajados = float(input("Quantos km você viajou? "))
 
     def custo_viagem():
-        # variáveis
-        viagem = 20
 
         if km_viajados < 0:
             print ("Por favor digite números positivos.")
         elif km_viajados < 50:
             print ("Não calculamos valores menores que 50km.")
         elif km_viajados <= 200:
-            print ("Olá,"  + nome + "!"" Sua viagem custará R$ ", km_viajados + viagem * 0.75)
+            print ("Olá,"  + nome + "!"" Sua viagem custará R$ ", viagem + km_viajados * 0.75)
         elif km_viajados <= 500:
-            print ("Olá,"  + nome + "!"" Sua viagem custará R$ ", km_viajados + viagem * 0.60)
+            print ("Olá,"  + nome + "!"" Sua viagem custará R$ ", viagem + km_viajados * 0.65)
         else:
-            print ("Olá,"  + nome + "!"" Sua viagem custará R$ ", km_viajados + viagem * 0.60)
+            print ("Olá,"  + nome + "!"" Sua viagem custará R$ ", viagem + km_viajados * 0.50)
 
     custo_viagem()
 
--- exercicios_casa/custo_viagem_try.py
+++ exercicios_casa/custo_viagem_try.py
@@ -2,23 +2,22 @@ print ("Bem-vindo ao nosso sistema de cálculo de custo de viagem.")
 
 # entradas
 nome = input("Qual seu nome? ")
+# variáveis
+viagem = 20
 
 try:
     km_viajados = float(input("Quantos km você viajou? "))
-    
-    # variáveis
-    viagem = 20
 
     if km_viajados < 0:
         print ("Por favor digite números positivos.")
     elif km_viajados < 50:
         print ("Não calculamos valores menores que 50km.")
     elif km_viajados <= 200:
-        print ("Olá,"  + nome + "!"" Sua viagem custará R$ ", km_viajados + viagem * 0.75)
+        print ("Olá,"  + nome + "!"" Sua viagem custará R$ ", viagem + km_viajados * 0.75)
     elif km_viajados <= 500:
-        print ("Olá,"  + nome + "!"" Sua viagem custará R$ ", km_viajados + viagem * 0.60)
+        print ("Olá,"  + nome + "!"" Sua viagem custará R$ ", viagem + km_viajados * 0.60)
     else:
-        print ("Olá,"  + nome + "!"" Sua viagem custará R$ ", km_viajados + viagem * 0.60)
+        print ("Olá,"  + nome + "!"" Sua viagem custará R$ ", viagem + km_viajados * 0.50)
 
 except:
     print ("Entre com um valor válido.")
